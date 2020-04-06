0e43d4b77a402a327d3f280b6dce8c36e8b4820c
Sho <tpkoni1010@gmail.com>
2020-04-06 11:02:00 +0700

    Initial Commit of Infrastructure.Data Project Update MVC for new UniversityDBContext

---
+++ CleanArch/ClearnArch.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,18 @@
+﻿using ClearnArch.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearnArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ CleanArch/ClearnArch.Domain/Models/Course.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClearnArch.Domain.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Decription { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
